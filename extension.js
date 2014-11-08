e59a23449fa8c069cbcf6e886c268bf3e12c6fdd
SArehalli <sgarehal@ucsd.edu>
2014-11-08 15:36:17 -0800

    Updated description.

--- extension.js
+++ extension.js
@@ -1,4 +1,10 @@
 $(document).ready(function() {
+  
+  var love = "</a> <a href='/'><div class='modified' style='display: inline ;color: green;'>Lover </div>";
+  var hate = "</a> <a href='/'><div class='modified' style='display: inline ;color: red;'>Hater </div>";
+  var switzerland = "</a> <a><div class='modified' style='display: inline ;color: grey;'>Neutral </div>";
+  
+  
   var completed = { '-1' : true};
   // Run on initial page
   setNames(document.getElementsByClassName("username js-action-profile-name"));
@@ -25,25 +31,25 @@ $(document).ready(function() {
           if ( status == "success" ) {
             memo[names[this.i].textContent] = data;
             if (Number(data) < -0.1) { 
-              names[this.i].innerHTML += "<div class='modified' style='display: inline ;color: red;'> Hater </div>";
+              names[this.i].innerHTML += hate; 
             }
             else if (Number(data) > 0.1) {
-              names[this.i].innerHTML += "<div class='modified' style='display: inline ;color: green;'> Lover </div>";
+              names[this.i].innerHTML += love; 
             }
             else {
-              names[this.i].innerHTML += "<div class='modified' style='display: inline ;color: grey;'> Neutral </div>";
+              names[this.i].innerHTML += switzerland;
             } 
           }
         };
         if (names[i] in memo) {
           if (Number(memo[names[i].textContent]) < -0.1) { 
-            names[i].innerHTML += "<div class='modified' style='display: inline ;color: red;'> Hater </div>";
+            names[i].innerHTML += hate; 
           }
           else if (Number(memo[names[i].textContent]) > 0.1) {
-            names[i].innerHTML += "<div class='modified' style='display: inline ;color: green;'> Lover </div>";
+            names[i].innerHTML += love; 
           }
           else {
-            names[i].innerHTML += "<div class='modified' style='display: inline ;color: grey;'> Neutral </div>";
+            names[i].innerHTML += switzerland; 
           } 
         }
         else { 
