f04f95ead174c39e7e5ef20d7ae3aae5988652c7
SArehalli <sgarehal@ucsd.edu>
2014-11-08 03:13:22 -0800

    This looks real now

--- extension.js
+++ extension.js
@@ -1,2 +1,26 @@
-var names = document.getElementsByClassName("username");
-alert(names);
+$(document).ready(function() {
+  // Run on initial page
+  setNames(document.getElementsByClassName("username js-action-profile-name"));
+  
+  // Create Observer
+  var observer = new MutationObserver(function(mutations) {
+    console.log("hey");
+    mutations.forEach( function(mutation) {
+      setNames(mutation.addedNodes);
+      mutation.addedNodes.forEach( function(node) {
+        setNames(node.getElementsByClassName("username js-action-profile-name"));
+      });
+    });
+  });
+  
+  var config = { attributes: false, childList: true, characterData: false };
+
+  observer.observe(document, config);
+
+  // Function to run on page
+  function setNames(names) {
+    for (var i = 0; i < names.length; i++) { 
+       names[i].textContent += " Hater?";
+    }
+  }
+});
